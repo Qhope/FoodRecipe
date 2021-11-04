85a1dc7a6f7dc0de071b39114bef3113e3a6953a
Qhope <vuquanghopvn2001@gmail.com>
2021-11-04 15:21:05 +0700

    Init v2

--- food-recipe-fe/src/App.js
+++ food-recipe-fe/src/App.js
@@ -1,8 +1,7 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Navbar from "./Component/NavBar";
 import Home from "./Component/Home";
-import SearchBar from "./Component/SearchBar";
-import FoodData from "./full_format_recipes.json"
+
 import "./css/App.css";
 function App() {
   return (
@@ -12,8 +11,6 @@ function App() {
         <Switch>
           <Route path="/Home" component={Home} exact={true} />
         </Switch>
-        
-        <SearchBar placehodler="Enter food name..." data={FoodData}/>
       </div>
     </BrowserRouter>
   );
