950d00c9dc12177abae94be2a63206a81a122a1e
Qhope <vuquanghopvn2001@gmail.com>
2021-11-22 10:40:05 +0700

    Tạo Backend

--- food-recipe-fe/src/App.js
+++ food-recipe-fe/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Navbar from "./Component/NavBar";
 import Home from "./page/Home";
+import Detail from "./page/Detail_Recipe";
 import "./css/App.css";
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Navbar />
         <Switch>
           <Route path="/Home" component={Home} exact={true} />
+          <Route path="/Detail" component={Detail} exact={true} />
         </Switch>
       </div>
     </BrowserRouter>
--- food-recipe-fe/src/Component/HomeComponent/SearchSection.js
+++ food-recipe-fe/src/Component/HomeComponent/SearchSection.js
@@ -1,12 +1,20 @@
+import React from "react";
 import "./SearchSection.scss";
+import { useHistory } from "react-router-dom";
 import { Card, Button, Input } from "antd";
 import { AiOutlineHeart, AiOutlineLike } from "react-icons/ai";
 const { Meta } = Card;
+
 const a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const { Search } = Input;
-const SearchSection = () => {
+const SearchSection = (props) => {
+  const history = useHistory();
   const check = () => {
     console.log("click");
+  };
+  const detail = (e) => {
+    console.log("click");
+    history.push("/Detail");
   };
   return (
     <div>
@@ -14,8 +22,9 @@ const SearchSection = () => {
         <Search />
       </div>
       <div className="card">
-        {a.map(() => (
+        {a.map((i) => (
           <Card
+            key={i}
             hoverable
             style={{ width: 240 }}
             cover={
@@ -24,6 +33,7 @@ const SearchSection = () => {
                 src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
               />
             }
+            onClick={detail}
           >
             <Meta
               title="Europe Street beat"
